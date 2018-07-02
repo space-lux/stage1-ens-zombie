2edf4c80907ace93358a5cda5dc1df2570655fb8
Alexandre FAYE <alexandre.faye-bedrin@ens-rennes.fr>
2018-07-02 09:27:06 +0200

    correction de la règle on_delete

--- munin/models.py
+++ munin/models.py
@@ -13,15 +13,15 @@ class TypeAgent(models.Model):
 
 class Joueur(models.Model):
 	nom=models.CharField(max_length=50,null=True,default=mot)
-	session=models.ForeignKey('sessions.Session',null=True,ondelete=models.CASCADE)
+	session=models.ForeignKey('sessions.Session',null=True,on_delete=models.CASCADE)
 	
 	def __str__(self):
 		return self.nom
 
 class Agent(models.Model):
 	nom=models.TextField(null=True,default=mot)
-	type_agent=models.ForeignKey('TypeAgent',null=True,ondelete=models.CASCADE)
-	joueur=models.ForeignKey('Joueur',null=True,ondelete=models.CASCADE)
+	type_agent=models.ForeignKey('TypeAgent',null=True,on_delete=models.CASCADE)
+	joueur=models.ForeignKey('Joueur',null=True,on_delete=models.CASCADE)
 	
 	def __str__(self):
 		return self.nom
@@ -29,4 +29,4 @@ class Agent(models.Model):
 class Ordre(models.Model):
 	prix=models.CharField(max_length=50,default="0")
 	puissance=models.CharField(max_length=50,default="0")
-	agent=models.ForeignKey('Agent',null=True,ondelete=models.CASCADE)
+	agent=models.ForeignKey('Agent',null=True,on_delete=models.CASCADE)
